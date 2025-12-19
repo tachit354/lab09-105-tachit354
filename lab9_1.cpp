70cbe4d4001b96ac74d43e83c2260bc142b9a68c
tachit354 <tachit_th@cmu.ac.th>
2025-12-19 17:50:57 +0700

    finish

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double money1,Interest,money2 ; //(ต้นทุน,ดอกเบี้ย,เงินจ่ายคืนได้ตอนสิ้นปี)
 	cout << "Enter initial loan: ";
+	cin >> money1;
 	cout << "Enter interest rate per year (%): ";
+	cin >> Interest;
 	cout << "Enter amount you can pay per year: ";
+	cin >> money2;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,25 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+
+	for(int i = 1; i > 0; i++){	
+		double b = money1 * (Interest/100);
+		double total = money1 + b;
+		double d = total - money2;
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << money1;
+	cout << setw(13) << left << b;
+	cout << setw(13) << left << total;
+	if(money2 < total){
+	cout << setw(13) << left << money2;
+	}else cout << setw(13) << left << total;
+	if(total - money2 > 0) cout << setw(13) << left << d;
+	else cout << setw(13) << left << 0.00;
+	cout << "\n";
+	money1 = d;
+	if(d < 0) break;
+	}	
 	
 	return 0;
 }
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int x,int y){
+	if(x > 0 && y > 0){
+		int i = 0;
+		while(i < x){
+			int j = 0;
+			while (j < y){
+				cout << "O";
+				j++;
+			}
+			cout << "\n";
+			i++;
+		}
+	}else cout << "Invalid input";
+}
 
 int main(){
 	
